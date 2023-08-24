47b59fdf5b8d35a9dc64980ddf0e5a2fb7e5ab79
kynkner <101127768+kynkner@users.noreply.github.com>
2023-08-24 18:49:15 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let container = document.getElementById('container');
+let count = 80;
+for(var i = 0; i < 80; i++){
+    let leftSnow = Math.floor(Math.random() * container.clientWidth);
+    let topSnow = Math.floor(Math.random() * container.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 60);
+    let timeSnow = Math.floor((Math.random() * 4) + 4);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(topSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container.appendChild(div);
+}
